1b55955fda7a6df48f530bf4dd5ad01aadb04d61
hanwone <hanhw@jccatech.com>
2024-09-03 17:40:30 +0800

    修改资产时IP重复问题

--- src/main/java/com/jcca/web/asset/service/impl/AssetServiceImpl.java
+++ src/main/java/com/jcca/web/asset/service/impl/AssetServiceImpl.java
@@ -1574,13 +1574,21 @@ public class AssetServiceImpl extends ServiceImpl<AssetMapper, Asset> implements
         }
 
         Asset oldasset = this.getOneByAllIp(ip);
-        if (Objects.nonNull(oldasset)) {
+        if (Objects.nonNull(oldasset) && StringUtils.isEmpty(req.getId())) {
+            throw new ResultException(Integer.parseInt(AssetCollectTestVo.ERRO_CODE), "资产IP1已经被其他设备占用");
+        }
+
+        if (Objects.nonNull(oldasset) && !StringUtils.isEmpty(req.getId()) && !oldasset.getId().equals(req.getId())) {
             throw new ResultException(Integer.parseInt(AssetCollectTestVo.ERRO_CODE), "资产IP1已经被其他设备占用");
         }
 
         if (!StringUtils.isEmpty(ip2)) {
             oldasset = this.getOneByAllIp(ip2);
-            if (Objects.nonNull(oldasset)) {
+            if (Objects.nonNull(oldasset) && StringUtils.isEmpty(req.getId())) {
+                throw new ResultException(Integer.parseInt(AssetCollectTestVo.ERRO_CODE), "资产IP2已经被其他设备占用");
+            }
+
+            if (Objects.nonNull(oldasset) && !StringUtils.isEmpty(req.getId()) && !oldasset.getId().equals(req.getId())) {
                 throw new ResultException(Integer.parseInt(AssetCollectTestVo.ERRO_CODE), "资产IP2已经被其他设备占用");
             }
         }
