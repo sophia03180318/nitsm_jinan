3a57c70f97a64fbbc5e81d72a5831b75a1e2faa8
任雅媛 <>
2024-09-05 09:17:35 +0800

    机柜管理 横向索引纵向索引有歧义

--- src/main/resources/static/js/main/index.js
+++ src/main/resources/static/js/main/index.js
@@ -884,8 +884,8 @@ function showNodes(vertexs, editor, graph, parent,currentPage) {
 					y = vertex.nodeY == null ? (vertex.rowIndex - 1) * 32 : y;
 				}else{
 					style = "shape=RECTANGLE;fillColor=rgba(40, 122, 212, 0.3);strokeColor=#00acff;fontColor=#00ACFF;fontStyle=1;rounded=1;verticalLabelPosition=center";
-					x = vertex.nodeX == null ? 108 * (vertex.columnIndex-1) + 15 : x;
-					y = vertex.nodeY == null ? 26 * (vertex.rowIndex - 1) + 15*(vertex.rowIndex - 1) +15  : y;
+					x = vertex.nodeX == null ? 108 * (vertex.rowIndex-1) + 15* (vertex.rowIndex-1) + 15 : x;
+					y = vertex.nodeY == null ? 26 * (vertex.columnIndex - 1) + 15  : y;
 				}
 
 
