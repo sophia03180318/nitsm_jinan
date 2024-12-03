7ad5d4d625fb9cc32b4481f26e62abd8a0768723
hanwone <hanhw@jccatech.com>
2024-12-03 10:37:23 +0800

    修改拓扑页签为枚举类型

--- src/main/java/com/jcca/web2/enums/TopoCategoryEnum.java
+++ src/main/java/com/jcca/web2/enums/TopoCategoryEnum.java
@@ -23,8 +23,9 @@ public enum TopoCategoryEnum {
     CABINET_TOPO("cabinet_topo", "机柜拓扑"),
     BIZ_TOPO("biz_topo", "业务拓扑"),
     PC_TOPO("pc_topo", "调度台拓扑"),
-    NET_WORKASSET_TOPO("netWorkAsset_topo", "网络设备资产连线拓扑"),
-    WAN_TOPO("wan_topo", "广域网拓扑图"),
+    NET_WORKASSET_TOPO("netWorkAsset_topo", "资产连线拓扑"),
+    WAN_TOPO("wan_topo", "广域网拓扑"),
+    PRIMARY_ROUTE_TOPO("primary_route_topo", "核心路由拓扑"),
 
     ;
 
