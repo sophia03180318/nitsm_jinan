8dfdf891ad272998f4ba1ab6c1e4a54efecd30c2
hanhw <hanhw@jccatech.com>
2025-02-14 09:25:50 +0800

    广域网拓扑BUG

--- src/main/java/com/jcca/web2/controller/GraphControllerV2.java
+++ src/main/java/com/jcca/web2/controller/GraphControllerV2.java
@@ -33,17 +33,14 @@ import com.jcca.web.asset.service.AssetService;
 import com.jcca.web.asset.service.CabinetService;
 import com.jcca.web.collect.controller.route.bean.AssetLinkConst;
 import com.jcca.web.collect.entity.CollectInterfaces;
-import com.jcca.web.collect.entity.CollectNetworkCard;
 import com.jcca.web.collect.service.*;
 import com.jcca.web.graph.entity.TopoAssetGroup;
 import com.jcca.web.graph.entity.TopoAssetMark;
 import com.jcca.web.graph.entity.TopoEdge;
 import com.jcca.web.graph.entity.TopoVertex;
 import com.jcca.web.graph.service.*;
-import com.jcca.web.graph.vo.TopoPortInfoVo;
 import com.jcca.web.graph.vo.TopoVertexAlarmLevelVo;
 import com.jcca.web.graph.vo.TopoVertexVo;
-import com.jcca.web.ip.service.NetWorkAddressService;
 import com.jcca.web2.entity.BusinessServiceType;
 import com.jcca.web2.entity.TopoTag;
 import com.jcca.web2.enums.TopoCategoryEnum;
@@ -52,7 +49,6 @@ import com.jcca.web2.service.TopoTagService;
 import com.jcca.web2.vo.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.compress.archivers.zip.X7875_NewUnix;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.CollectionUtils;
@@ -729,16 +725,20 @@ public class GraphControllerV2 {
         QueryWrapper<Asset> query = Wrappers.query();
         query.eq("DESK", AssetModeConst.ROUTER);
         query.eq("SHOW_TOPO", 1);
-        query.eq("IS_DEL",1);
+        query.eq("IS_DEL", 1);
         query.in("ORG_ID", orgIds);
         List<Asset> assetList = assetService.list(query);
         query = Wrappers.query();
         query.eq("DESK", AssetModeConst.ROUTER);
         query.eq("SHOW_TOPO", 1);
-        query.eq("IS_DEL",1);
+        query.eq("IS_DEL", 1);
         query.eq("SHOW_CORE", "SHOW_TOPO_@_SHOW");
         query.eq("ORG_ID", orgId);
         assetList.addAll(assetService.list(query));
+        if (assetList.isEmpty()) {
+            map.put("vertex", new ArrayList<>());
+            return;
+        }
 
         List<String> ids = assetList.stream().map(Asset::getId).collect(Collectors.toList());
         List<TopoVertexVo> list = topoVertexService.selectWanTopoNodeByAsset(category, ids);
