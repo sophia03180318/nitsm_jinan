eb1f243d05c1ca63854164de696fb96e14c71181
hanhw <hanhw@jccatech.com>
2025-03-17 16:57:50 +0800

    电源模块解说

--- src/main/java/com/jcca/dataProcessing/manager/impl/DataChangeMangerService.java
+++ src/main/java/com/jcca/dataProcessing/manager/impl/DataChangeMangerService.java
@@ -152,7 +152,7 @@ public class DataChangeMangerService implements IDataChangeManagerService {
         alarmInfo.setType(AlarmTypeEnum.HARDWARE.getCode());
 
         alarmInfo.setContent(content);
-        if ((content.contains("Redundancy") && content.contains("Lost")) || content.contains("Non-redundant")) {
+        if ((content.contains("Non-redundant") && content.contains("deasserted"))) {
             alarmInfo.setContent("电源模块冗余电源丢失");
         }
         alarmInfo.setDescription(event.getDescStr());
