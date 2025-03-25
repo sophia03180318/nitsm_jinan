e033eb6cbca571c6988dbd756130fa0dbb680d3b
hanhw <hanhw@jccatech.com>
2025-03-25 17:42:11 +0800

    还原代码

--- src/main/java/com/jcca/dataProcessing/DataFilter/process/ProcessSaveGroupFilterHandler.java
+++ src/main/java/com/jcca/dataProcessing/DataFilter/process/ProcessSaveGroupFilterHandler.java
@@ -1,7 +1,5 @@
 package com.jcca.dataProcessing.DataFilter.process;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.jcca.common.enums.StatusEnum;
 import com.jcca.common.log.enums.LogFunctionEnum;
 import com.jcca.common.utils.AppLogUtils;
@@ -11,7 +9,6 @@ import com.jcca.dataProcessing.support.IFilterHandler;
 import com.jcca.web.asset.entity.ThresholdProcess;
 import com.jcca.web.asset.service.ThresholdProcessService;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -34,18 +31,8 @@ public class ProcessSaveGroupFilterHandler extends IFilterHandler<ProcessGroupEn
     public boolean handler(ProcessGroupEntity info) {
         AppLogUtils.buildLogInfo(LogFunctionEnum.DATA_PROCESS_SINGLE, "保存组进程数据", info.getAssetIp());
         List<ProcessAlarmQueueEntity> queueObj = info.getQueueObj();
-        QueryWrapper<ThresholdProcess> query;
         for (ProcessAlarmQueueEntity processAlarmQueueEntity : queueObj) {
-            String assetId = processAlarmQueueEntity.getAssetId();
-            String processName = processAlarmQueueEntity.getProcessName();
-            query = Wrappers.query();
-            query.eq("ASSET_ID", assetId);
-            query.eq("PROCESS_NAME", processName);
-            List<ThresholdProcess> list = thresholdService.list(query);
-            if (CollectionUtils.isEmpty(list)) {
-                continue;
-            }
-            ThresholdProcess threshold = list.get(0);
+            ThresholdProcess threshold = thresholdService.getById(processAlarmQueueEntity.getThresholdId());
             threshold.setProcessId(processAlarmQueueEntity.getProcessId());
             threshold.setCollectStatus(processAlarmQueueEntity.getProcessStatus() ? StatusEnum.OK.getCode() : StatusEnum.NO.getCode());
             thresholdService.updateById(threshold);
