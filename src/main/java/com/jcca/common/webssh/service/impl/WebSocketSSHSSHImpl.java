e374a3f223806e7a0d6776a67e260ecbe4745b98
hanhw <hanhw@jccatech.com>
2025-04-14 15:20:59 +0800

    增加心跳日志

--- src/main/java/com/jcca/common/webssh/service/impl/WebSocketSSHSSHImpl.java
+++ src/main/java/com/jcca/common/webssh/service/impl/WebSocketSSHSSHImpl.java
@@ -112,7 +112,7 @@ public class WebSocketSSHSSHImpl implements WebSocketSSHService {
                 }
             }
         } else if (ConstantPool.WEBSSH_OPERATE_HEARTBEAT.equals(webRemoteData.getOperate())) {
-            AppLogUtils.buildLogError(LogFunctionEnum.REMOTE_CONNECT, "SSH心跳消息", ConstantPool.WEBSSH_OPERATE_HEARTBEAT);
+            AppLogUtils.buildLogInfo(LogFunctionEnum.REMOTE_CONNECT, "SSH心跳消息", ConstantPool.WEBSSH_OPERATE_HEARTBEAT);
             try {
                 sendMessage(session, "OK".getBytes());
             } catch (IOException e) {
