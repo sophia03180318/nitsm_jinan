8baf3bc069af100ef94460908a2d7241a079958f
lvyp <874964727@qq.com>
2025-06-11 10:38:37 +0800

    增加两个类

---
+++ src/main/java/com/jcca/dataProcessing/Entity/CollectResultItemEntity.java
@@ -0,0 +1,17 @@
+package com.jcca.dataProcessing.Entity;
+
+import lombok.Data;
+
+/**
+ * 结果
+ */
+@Data
+public class CollectResultItemEntity {
+
+    private String eventKey;
+
+    private Boolean status;
+
+
+
+}
