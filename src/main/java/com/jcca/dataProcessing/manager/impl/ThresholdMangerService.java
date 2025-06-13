7e655fb4d2d6a6100c3b755960d47144b215eab8
lvyp <874964727@qq.com>
2025-06-13 19:29:14 +0800

    增加获取阈值方法

--- src/main/java/com/jcca/dataProcessing/manager/impl/ThresholdMangerService.java
+++ src/main/java/com/jcca/dataProcessing/manager/impl/ThresholdMangerService.java
@@ -426,7 +426,25 @@ public class ThresholdMangerService implements ThresholdManager {
 
     @Override
     public ThresholdBaseEntity getThresholdValue(String code, String assetId, String flag) {
+
         return v2(code, assetId, flag);
+    }
+
+    @Override
+    public ThresholdBaseEntity xunjianGetThresholdValue(String code, String assetId, String flag) {
+        String newCode = "";
+        if(code.contains("section")){
+            newCode = code.replace("section", "normal");
+        }else if(code.contains("sectionOne")){
+            newCode = code.replace("sectionOne", "normal");
+        }else if(code.contains("sectionTwo")){
+            newCode = code.replace("sectionTwo", "normal");
+        }else if(code.contains("sectionThree")){
+            newCode = code.replace("sectionThree", "normal");
+        }
+
+
+        return v2(newCode, assetId, flag);
     }
 
     /**
