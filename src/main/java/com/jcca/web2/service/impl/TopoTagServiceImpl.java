675d6927539e2b713a20d5cd117cd3242dd53cfc
hanhw <hanhw@jccatech.com>
2025-06-25 17:31:31 +0800

    正式签

--- src/main/java/com/jcca/web2/service/impl/TopoTagServiceImpl.java
+++ src/main/java/com/jcca/web2/service/impl/TopoTagServiceImpl.java
@@ -54,10 +54,10 @@ public class TopoTagServiceImpl extends ServiceImpl<TopoTagMapper, TopoTag> impl
             topoTag.setId(MyIdUtil.getId());
             topoTag.setRemark("前端创建");
         }
-        int tmp = 0;
+        int tmp = 1;
         List<TopoTag> nlist = new ArrayList<>();
         if ("0".equals(preId)) {
-            topoTag.setTagSort(tmp++);
+            topoTag.setTagSort(tmp);
             nlist.add(topoTag);
         }
         for (TopoTag existingTag : existingTags) {
