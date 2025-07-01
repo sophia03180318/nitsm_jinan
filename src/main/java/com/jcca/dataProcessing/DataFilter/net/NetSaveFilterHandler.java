0b91c3b3d13e620ed7cb9668a404d19a50fc3979
lvyp <874964727@qq.com>
2025-07-01 14:54:52 +0800

    删除过滤无IP网卡逻辑，通过后面的变动逻辑自动过滤

--- src/main/java/com/jcca/dataProcessing/DataFilter/net/NetSaveFilterHandler.java
+++ src/main/java/com/jcca/dataProcessing/DataFilter/net/NetSaveFilterHandler.java
@@ -36,30 +36,6 @@ public class NetSaveFilterHandler extends IFilterHandler<CollectNetworkCardEntit
     @Override
     public synchronized boolean handler(CollectNetworkCardEntity info) {
         AppLogUtils.buildLogInfo(LogFunctionEnum.DATA_PROCESS_SINGLE, "保存网卡信息", info.getAssetIp());
-        //过滤掉IP地址为空的网卡 同时组的还要上
-        String ip = info.getIp();
-
-        // 断网后有的网卡采集不到IP 使用原有IP
-        if (DEFAULT_VALUE_STR.equals(ip)) {
-            QueryWrapper<CollectNetworkCard> query = Wrappers.query();
-            query.eq("ASSET_ID", info.getAssetId());
-            query.eq("NAME", info.getName());
-            List<CollectNetworkCard> list = networkService.list(query);
-            if (!list.isEmpty()) {
-                info.setIp(list.get(0).getIp());
-            }
-        }
-
-        if (StrUtil.isEmpty(ip) || DEFAULT_VALUE_STR.equals(ip)) {
-            if (!StringUtils.isEmpty(info.getBondType()) && !info.getName().contains("WFP") && !info.getName().contains("QoS")) {
-                // 标记行
-            } else if (!info.getName().contains("组") || info.getName().contains("WFP") || info.getName().contains("QoS")) {
-                //过滤掉名字不包含组，或包含组 含有WFP、QoS的
-                return false;
-            }
-        }
-
-
 
         Date date = new Date();
         date.setTime(info.getCollectTime());
