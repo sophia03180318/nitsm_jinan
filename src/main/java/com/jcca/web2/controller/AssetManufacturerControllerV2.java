5bcfd2a2025042401d700851dc7277555bb01dda
lvyp <874964727@qq.com>
2025-07-14 14:28:21 +0800

    资产厂商ID处理

--- src/main/java/com/jcca/web2/controller/AssetManufacturerControllerV2.java
+++ src/main/java/com/jcca/web2/controller/AssetManufacturerControllerV2.java
@@ -23,6 +23,7 @@ import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @description: 资产厂商
@@ -116,7 +117,11 @@ public class AssetManufacturerControllerV2 {
         }
 
         Long id = manufacturerService.getMaxId();
-        assetManufacturer.setId(id + 1);
+        if(Objects.isNull(id)){
+            assetManufacturer.setId(1L);
+        }else{
+            assetManufacturer.setId(id + 1);
+        }
         manufacturerService.save(assetManufacturer);
         return ResultVoUtil.success("保存成功");
     }
