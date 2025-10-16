aceeb1f615a43103b74f1be6f39bb33b0316a385
张雅慧 <zhangyh@jccatech.com>
2025-10-16 09:41:04 +0800

    修改动环事件筛选条件

--- src/main/java/com/jcca/dataProcessing/listener/eventInfoHandler/EventIsFilterAlarmHandler.java
+++ src/main/java/com/jcca/dataProcessing/listener/eventInfoHandler/EventIsFilterAlarmHandler.java
@@ -28,7 +28,7 @@ import java.util.stream.Collectors;
 @Component("eventIsFilterAlarmHandler")
 public class EventIsFilterAlarmHandler extends IFilterHandler<IEvent> {
 
-    private static final String REPO_CODE = "event:event_environment:notify";
+    private static final String DH_CODE = "event:event_environment:notify";
 
     @Resource
     private DeviceService deviceService;
@@ -38,7 +38,7 @@ public class EventIsFilterAlarmHandler extends IFilterHandler<IEvent> {
         /**
          * 筛选动环设备
          */
-        if (!Objects.isNull(info.getEventAlarmLevelBaseEntity()) && REPO_CODE.equals(info.getEventAlarmLevelBaseEntity().getRepoId())) {
+        if (!Objects.isNull(info.getEventAlarmLevelBaseEntity()) && DH_CODE.equals(info.getEventAlarmLevelBaseEntity().getAlarmCode())) {
             EventAlarmLevelBaseEntity eventAlarmLevelBaseEntity = info.getEventAlarmLevelBaseEntity();
             String repoId = eventAlarmLevelBaseEntity.getRepoId();
             //调用方法 通过规则筛选设备
