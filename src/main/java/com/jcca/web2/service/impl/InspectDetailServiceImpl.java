8573babe669504880de219d5e784e1b1712e1d46
lifp <1936285896@qq.com>
2025-10-28 15:42:52 +0800

    feat: 将智能巡检调整为告警，筛选掉正常事件 (2.1.10.1)

--- src/main/java/com/jcca/web2/service/impl/InspectDetailServiceImpl.java
+++ src/main/java/com/jcca/web2/service/impl/InspectDetailServiceImpl.java
@@ -227,19 +227,20 @@ public class InspectDetailServiceImpl extends ServiceImpl<InspectDetailMapper, I
     }
 
     private String parseStatusFromTotalType(String totalType) {
-        switch (totalType) {
-            case Web2Const.TOTAL_TYPE_ABNORMAL:
-                return Web2Const.INSPECT_ERROR;
-            case Web2Const.TOTAL_TYPE_WARNING:
-                return Web2Const.INSPECT_ALARM;
-            default:
-                return "-1"; // 查询全部状态
-        }
+//        switch (totalType) {
+//            case Web2Const.TOTAL_TYPE_ABNORMAL:
+//                return Web2Const.INSPECT_ERROR;
+//            case Web2Const.TOTAL_TYPE_WARNING:
+//                return Web2Const.INSPECT_ALARM;
+//            default:
+//                return "-1"; // 查询全部状态
+//        }
+        return "5";
     }
 
     @Override
     public InspectTargetDetailInfoVo getTargetDetail(String inspectCode, String assetId, String type) {
-        List<InspectTargetDetailInfo> targetDetailInfoList = inspectDetailMapper.getTargetDetail(inspectCode, assetId,!StringUtils.isEmpty(type)?Web2Const.INSPECT_ALARM:"-1");
+        List<InspectTargetDetailInfo> targetDetailInfoList = inspectDetailMapper.getTargetDetail(inspectCode, assetId,Web2Const.INSPECT_ALARM);
         for (InspectTargetDetailInfo info : targetDetailInfoList) {
             info.setTargetType(info.getTargetItem().substring(0, info.getTargetItem().lastIndexOf(":")));
         }
