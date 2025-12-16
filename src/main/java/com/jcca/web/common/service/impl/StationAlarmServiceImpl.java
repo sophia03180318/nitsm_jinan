1e68d2ff419c94feb328cd91092d9bfd3071708b
lvyp <874964727@qq.com>
2025-12-16 16:24:26 +0800

    修复车站上送告警BUG

--- src/main/java/com/jcca/web/common/service/impl/StationAlarmServiceImpl.java
+++ src/main/java/com/jcca/web/common/service/impl/StationAlarmServiceImpl.java
@@ -430,7 +430,7 @@ public class StationAlarmServiceImpl implements StationAlarmService {
         alarmInfo.setOccurTime(occurTime);
         alarmInfo.setLastTime(occurTime);
         alarmInfo.setAlarmCode(STATION_ALARM_UNIQUE);
-        alarmInfo.setAlarmFlag(req.getFlag());
+        alarmInfo.setAlarmFlag(req.getAlarmCode());
         alarmInfo.setDescription(req.getAlarmDescription());
         alarmInfo.setContent(req.getAlarmDescription());
         alarmInfo.setBlank(blank);
